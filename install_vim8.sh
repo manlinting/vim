63137d265464af3707a3c75339c1ee947902b90d
root <root@241890.sys.ipv4.io>
2017-09-23 04:44:47 -0400

    add vim8

---
+++ install_vim8.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#########################################################################
+# Author: lincolnlin
+# Created Time: Sat Sep 23 04:39:19 2017
+# File Name: install_vim8.sh
+# Description: 
+#########################################################################
+
+
+git clone https://github.com/vim/vim.git --depth=1 && cd vim/
+
+./configure --prefix=/usr/local/vim8 --enable-multibyte  --with-tlib=tinfo  --enable-pythoninterp --with-features=huge
+
+make 
+
+make install
+
+alias vim='/usr/local/vim8/bin/vim'
